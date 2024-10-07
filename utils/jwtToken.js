37ddeb25e7114e6b3a4977de94ea064bc25b743e
viditjoshi <viditjoshi94@gmail.com>
2024-10-07 20:58:55 +0530

    token problem solve

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -1,16 +1,14 @@
-// Create Token and saving in cookie
-
 const sendToken = (user, statusCode, res) => {
   const token = user.getJWTToken();
 
-  // options for cookie
+  // Options for the cookie
   const options = {
     expires: new Date(
       Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
     ),
-    httpOnly: true, // Make sure this is set appropriately for your use case
-    secure: process.env.NODE_ENV === "production", // Set secure to true in production
-    sameSite: "lax", // Adjust sameSite setting if needed
+    httpOnly: true, // Prevents client-side access to the cookie for security
+    secure: process.env.NODE_ENV === "production", // Ensure the cookie is sent over HTTPS in production
+    sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax", // Use "None" for cross-site in production, otherwise "Lax"
   };
 
   res.status(statusCode).cookie("token", token, options).json({

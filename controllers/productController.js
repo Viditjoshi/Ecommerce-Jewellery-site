82bdfd4eaecf758fd3d738d555a2aecce1fe56df
Vidit Joshi <110344680+Viditjoshi@users.noreply.github.com>
2024-10-19 05:09:05 +0530

    Update productController.js

--- controllers/productController.js
+++ controllers/productController.js
@@ -36,7 +36,7 @@ exports.createProduct = catchAsyncErrors(async (req, res, next) => {
 
 // Get All Product
 exports.getAllProducts = catchAsyncErrors(async (req, res, next) => {
-  const resultPerPage = 8;
+  const resultPerPage = 20;
   const productsCount = await Product.countDocuments();
 
   const apiFeature = new ApiFeatures(Product.find(), req.query)
